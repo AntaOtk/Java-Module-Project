176cd01c592e945cca95a64dab89691d402219e9
AntaOtk <ovaorange@yandex.ru>
2022-11-29 19:28:41 +0300

    version after PR

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,23 +8,28 @@ public class Main {
     public static void main(String[] args) {
         int peoples = readCounter();
         ProductList products = new  ProductList();
-        products.readProducts();
+        products.readProducts(scanner);
         printConclusion(products, products.calculate(peoples));
+        scanner.close();
     }
 
     public static int readCounter() {
-        int n;
         while (true) {
             System.out.println("На скольких человек необходимо разделить счёт");
-            n = scanner.nextInt();
-            scanner.nextLine();
-            if (n > 1) {
-                return n;
-            }
-            if (n == 1) {
-                System.out.println("Нет смысла делить счёт");
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                scanner.nextLine();
+                if (number > 1) {
+                    return number;
+                }
+                if (number == 1) {
+                    System.out.println("Нет смысла делить счёт");
+                } else {
+                    System.out.println("Неверное количество людей");
+                }
             } else {
-                System.out.println("Неверное количество людей");
+                System.out.println("Введите пожалуйста число");
+                scanner.next();
             }
         }
     }
@@ -32,15 +37,7 @@ public class Main {
     public static void printConclusion(ProductList products, float output) {
         System.out.println("Добавленные товары:");
         products.writeNameList();
-        int rub = (int) output;
-        if ((rub % 10 == 1) && (rub / 10 != 1)) {
-            System.out.println(String.format("%.2f", output) + " рубль");
-        } else if ((rub % 10 >= 2) && (rub % 10 <= 4) && (rub / 10 != 1)) {
-            System.out.println(String.format("%.2f", output) + " рубля");
-        } else {
-            System.out.println(String.format("%.2f", output) + " рублей");
-        }
-
+        System.out.println(String.format("%.2f", output) + Spelling.writeRub(output));
     }
 
 }
--- src/main/java/ProductList.java
+++ src/main/java/ProductList.java
@@ -4,34 +4,31 @@ import java.util.Scanner;
 public class ProductList {
 
     private ArrayList<Product> productsList = new ArrayList<>();
-    private Scanner scanner = new Scanner(System.in);
 
-    public void readProducts() {
+    public void readProducts(Scanner scanner) {
         String end = "завершить";
-        String add1;
-        String add2;
-        for (; ; ) {
-            System.out.println("введите имя");
-            add1 = scanner.nextLine();
+        String nameOfProduct;
+        float costOfProduct;
+        while(true) {
+            System.out.println("введите имя товара");
+            nameOfProduct = scanner.nextLine();
             System.out.println("введите стоимость");
-            add2 = scanner.nextLine();
-            boolean err = false;
-            if (add2.length() > 3) {
-                for (int i = 0; i < add2.length(); i++) {
-                    if ((!Character.isDigit(add2.charAt(i))) && (i != add2.length() - 3)) {
-                        err = true;
-                    }
-                }
-                if ((add2.charAt(add2.length() - 3) == '.') && !err) {
-                    productsList.add(new Product(add1, Float.parseFloat(add2)));
-                    System.out.println("Товар успешно добавлен. Хотите ввсти ещё товар?");
+            if (scanner.hasNextFloat()) {
+                costOfProduct = scanner.nextFloat();
+                scanner.nextLine();
+                if (costOfProduct > 0) {
+                    productsList.add(new Product(nameOfProduct, costOfProduct));
+                    System.out.println("Товар успешно добавлен. Хотите ввести ещё товар? \nЕсли нет, то введите ''Завершить''");
                     if (end.equalsIgnoreCase(scanner.nextLine())) {
                         break;
                     }
                 } else {
                     System.out.println("Товар введён неверно");
                 }
-            } else System.out.println("Товар введён неверно");
+            } else {
+                System.out.println("Товар введён неверно");
+                scanner.nextLine();
+            }
         }
     }
 
